f2593cd5f2b420ad72734ee52f69147f81f85311
Kynica <dmitry.kozyuk@gmail.com>
2019-02-11 11:29:10 +0200

    Rename ActiveRecordRepository to AbstractActiveRecordRepository and make it abstract.

--- src/repositories/FileRepository.php
+++ src/repositories/FileRepository.php
@@ -1,10 +1,10 @@
 <?php
 namespace DmitriiKoziuk\yii2FileManager\repositories;
 
-use DmitriiKoziuk\yii2Base\repositories\ActiveRecordRepository;
+use DmitriiKoziuk\yii2Base\repositories\AbstractActiveRecordRepository;
 use DmitriiKoziuk\yii2FileManager\entities\File;
 
-class FileRepository extends ActiveRecordRepository
+class FileRepository extends AbstractActiveRecordRepository
 {
     /**
      * @param string $entityName
