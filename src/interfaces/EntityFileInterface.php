e6b06437614702d381dbda1371923da49eb0149e
Dmitrii Koziuk <dmitriikoziuk@gmail.com>
2019-11-27 14:49:56 +0200

    Remove non used interface

--- src/interfaces/EntityFileInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace DmitriiKoziuk\yii2FileManager\interfaces;
-
-interface EntityFileInterface
-{
-    public function getEntityName(): string;
-
-    public function getEntityId(): string;
-}
